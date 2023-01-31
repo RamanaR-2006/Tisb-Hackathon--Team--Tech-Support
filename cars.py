f438d02399bfc4ea9426d355224c84858d5edd8d
RamanaR-2006 <ramanaramachandran14@gmail.com>
2023-01-31 20:24:13 +0530

    commit

--- cars.py
+++ cars.py
@@ -8,7 +8,7 @@ def rescaleFrame(frame, scale=0.7):
 
     return cv.resize(frame, dimensions,interpolation=cv.INTER_AREA)
 
-img = cv.imread('cars4.jpg')
+img = cv.imread('cars_3.webp')
 cv.imshow('Car', img)
 
 scaleup = rescaleFrame(img)
