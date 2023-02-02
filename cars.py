b147a0663ae362e4a224a6e35aa9f1f5afadbe6d
siddharthagr6 <86679543+siddharthagr6@users.noreply.github.com>
2023-02-02 10:14:13 +0530

    Add files via upload

--- cars.py
+++ cars.py
@@ -1,37 +1,41 @@
 import cv2 as cv
 import numpy as np
 
-def rescaleFrame(frame, scale=0.7):
+def rescaleFrame(frame, scale=1.5):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dimensions = (width, height)
 
     return cv.resize(frame, dimensions,interpolation=cv.INTER_AREA)
+capture = cv.VideoCapture(r"C:\Users\siddharth\Desktop\TISBHACKS\GUI\dataset_video1 (1).avi")
 
-img = cv.imread('cars_3.webp')
-cv.imshow('Car', img)
+while True:
+    isTrue, Frame = capture.read()
+    scaleup = rescaleFrame(Frame)
+    #cv.imshow('Scaled up', scaleup)
 
-scaleup = rescaleFrame(img)
-cv.imshow('Scaled up', scaleup)
+    gray = cv.cvtColor(scaleup, cv.COLOR_BGR2GRAY)
+    #cv.imshow('Grayscale Car', gray)
 
-gray = cv.cvtColor(scaleup, cv.COLOR_BGR2GRAY)
-cv.imshow('Grayscale Car', gray)
+    blur = cv.GaussianBlur(gray,(5,5),0)
+    #cv.imshow('Blur', blur)
+    dilated = cv.dilate(blur,np.ones((3,3)))
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 2))
+    closing = cv.morphologyEx(dilated, cv.MORPH_CLOSE, kernel) 
 
-blur = cv.GaussianBlur(gray,(5,5),0)
-cv.imshow('Blur', blur)
+    car_cascade_src = 'cars.xml'
+    car_cascade = cv.CascadeClassifier(car_cascade_src)
+    cars = car_cascade.detectMultiScale(closing, 1.1, 1)
 
-dilated = cv.dilate(blur,np.ones((3,3)))
-kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 2))
-closing = cv.morphologyEx(dilated, cv.MORPH_CLOSE, kernel) 
+    print(len(cars))
+    no = len(cars)
 
-car_cascade_src = 'cars.xml'
-car_cascade = cv.CascadeClassifier(car_cascade_src)
-cars = car_cascade.detectMultiScale(closing, 1.1, 1)
+    for (x, y, w, h) in cars:
+        cv.rectangle(scaleup, (x,y), (x+w, y+h), (0,255,0), thickness=1)
 
-print(len(cars))
+    cv.imshow('Detected Cars', scaleup)
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
 
-for (x, y, w, h) in cars:
-    cv.rectangle(scaleup, (x,y), (x+w, y+h), (0,255,0), thickness=1)
-
-cv.imshow('Detected Cars', scaleup)
-cv.waitKey(0)
+capture.release()
+cv.destroyAllWindows
